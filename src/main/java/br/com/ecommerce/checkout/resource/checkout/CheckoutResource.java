806552c01b1b90c9568849e9352793c469fef2db
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-09-24 14:52:17 -0300

    Configura CheckoutRequest e Adiciona CheckoutResource

---
+++ src/main/java/br/com/ecommerce/checkout/resource/checkout/CheckoutResource.java
@@ -0,0 +1,21 @@
+package br.com.ecommerce.checkout.resource.checkout;
+
+import br.com.ecommerce.checkout.event.CheckoutCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/checkout")
+@RequiredArgsConstructor
+public class CheckoutResource {
+
+    @PostMapping("/")
+    public ResponseEntity<Void> create(CheckoutRequest checkoutRequest){
+        return ResponseEntity.ok().build();
+    }
+
+}
